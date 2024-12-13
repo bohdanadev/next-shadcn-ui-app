a8a0dc57d210f4bb83ff8f3f4f83ee45ee78ad30
Bohdana K <bohdana.dev@gmail.com>
2024-12-13 14:35:12 +0200

    Add landingPage, light-dark toggle

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { PersonStandingIcon } from 'lucide-react';
+
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <h1 className='flex gap-2 items-center'>
+        <PersonStandingIcon size={50} className='text-pink-500' /> SupportMe
+      </h1>
+      <p>The best dashboard to manage customer support</p>
+      <div className='flex gap-2 items-center'>
+        <Button asChild>
+          <Link href='/login'>Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button asChild variant='outline'>
+          <Link href='/sign-up'>Sign up</Link>
+        </Button>
+      </div>
+    </>
   );
 }
