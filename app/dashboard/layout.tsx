53cb662dafa356816a69f0b097d1242757c8088e
Bohdana K <bohdana.dev@gmail.com>
2024-12-16 15:39:43 +0200

    Add dashboard with cards and charts

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import MainMenu from './components/main-menu';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='md:grid md:grid-cols-[250px_1fr] h-screen'>
+      <MainMenu />
+      <div className='overflow-auto py-2 px-4'>
+        <h1 className='pb-4'>Welcome back, John!</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
